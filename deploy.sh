b0e0f8b72b77c177f86e8ee15612ba1707d48ccf
gbxnga <iamblizzyy@gmail.com>
2020-12-14 17:58:11 +0100

    uu

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,5 @@
 MESSAGE=$1
 git add .
 git commit -m "$MESSAGE"
-git push
-php artisan config:clear
-php artisan apidoc:generate
-sls deploy --aws-profile budgit
+git push 
+SLS_DEBUG=* sls deploy --aws-profile budgit
